715208a16f807e31d1bc1ce2afa759f7072ccdb2
Walter <walteraandrade@gmail.com>
2020-05-06 08:12:43 -0300

    Commit com o footer para o MVP

--- src/Containers/HomePage/index.js
+++ src/Containers/HomePage/index.js
@@ -1,28 +1,33 @@
-import React from 'react'
-import {HomeContainer, HeaderContainer, BannerContainer, ProdutosContainer, ProdutoItem} from './style'
-import Header from '../../components/Header'
+import React from "react";
+import {
+  HomeContainer,
+  HeaderContainer,
+  BannerContainer,
+  ProdutosContainer,
+  ProdutoItem,
+} from "./style";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
-class HomePage extends React.Component{
-    render(){
-        return(
-            <HomeContainer>
+class HomePage extends React.Component {
+  render() {
+    return (
+      <HomeContainer>
+        <HeaderContainer>
+          <Header tituloTeste="Funcionou"></Header>
+        </HeaderContainer>
 
-                <HeaderContainer>
-                    <Header tituloTeste="Funcionou"></Header>
-                </HeaderContainer>
+        <BannerContainer></BannerContainer>
 
-                <BannerContainer></BannerContainer>
-
-                <ProdutosContainer>
-                    <ProdutoItem></ProdutoItem>
-                    <ProdutoItem></ProdutoItem>
-                    <ProdutoItem></ProdutoItem>
-                </ProdutosContainer>
-
-            </HomeContainer>
-        )
-    }
-
+        <ProdutosContainer>
+          <ProdutoItem></ProdutoItem>
+          <ProdutoItem></ProdutoItem>
+          <ProdutoItem></ProdutoItem>
+        </ProdutosContainer>
+        <Footer />
+      </HomeContainer>
+    );
+  }
 }
 
-export default HomePage
+export default HomePage;
