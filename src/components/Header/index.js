401dd6c62ba2ac89bbbeec04d08559bf42b6838c
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-05-06 18:12:06 -0300

    inicio da estilização do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import {BotaoContainer} from './style'
-import FacebookIcon from '@material-ui/icons/Facebook';
+import {HeaderFlex, ContainerLogo, InputBusca, IconeCarrinho} from './style';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import Logo from '../../Logo/logo.png'
+import InputAdornment from '@material-ui/core/InputAdornment'; 
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
 
-class Header extends React.Component{
-    render(){
-        return(
-            <div>
-                <BotaoContainer>
-                    <FacebookIcon></FacebookIcon>
-                </BotaoContainer>
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <HeaderFlex>
+                <ContainerLogo src={Logo} alt="Logo 4user"/>
+                <div>
+                    <FormControl>
+                        <TextField
+                            label="Perquisar"
+                            variant="outlined"
+                            id="custom-css-outlined-input"
+                        />
+                    </FormControl>
+                </div>
                 
-            </div>
+                <Fab color="primary" aria-label="Add">
+                    <PersonOutlineOutlinedIcon />
+                </Fab>
+                
+                <IconButton>
+                    <Fab color="primary" >
+                        <Badge badgeContent={1}>
+                            <ShoppingCartOutlinedIcon />
+                        </Badge>
+                    </Fab>
+                    
+                </IconButton>
+              
+            </HeaderFlex>
         )
     }
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,8 +1,27 @@
 import styled from 'styled-components'
 import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input'; 
+
+export const InputBusca = styled(Input)`
+    background-color: #a5c1ce;
+    border-radius: 10px;
+    :hover{
+        background-color: #BED2DC;
+    }
+`
 
 export const BotaoContainer = styled(IconButton)`
     &&{
         color: purple;
     }
+`
+
+export const HeaderFlex = styled.div`
+    display: flex;
+    background-color: #a5c1ce;
+`
+
+export const ContainerLogo = styled.img`
+    height: 70px;
+    width: auto;
 `
