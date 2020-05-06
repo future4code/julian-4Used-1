77f0f00e9a20bd3f65f924167d74b00b56f55030
Rafael Cardoso <rafafscardoso@gmail.com>
2020-05-06 15:22:55 -0300

    Estrutaração da tela de login

--- src/Containers/Login/index.js
+++ src/Containers/Login/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import {} from './style'
+import React from 'react';
+import {
+    LoginContainer,
+    HeaderContainer,
+    FormContainer
+} from './style';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Login extends React.Component{
     render(){
         return(
-            <div>
-                Login
-            </div>
+            <LoginContainer>
+                <HeaderContainer />
+                <FormContainer>
+                    <TextField label='Nome do usuário' />
+                    <TextField label='Password' type='password' />
+                    <Button>Entrar</Button>
+                </FormContainer>
+                <Footer />
+            </LoginContainer>
         )
     }
 }
