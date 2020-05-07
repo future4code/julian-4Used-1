95b0d98deb7c0b9e0cb9af28660953ae9d782e95
Walter <walteraandrade@gmail.com>
2020-05-07 09:45:24 -0300

    mudanças no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import {HeaderFlex, ContainerLogo, InputBusca, IconeCarrinho} from './style';
-import SearchIcon from '@material-ui/icons/Search';
-import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
-import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import Logo from '../../Logo/logo.png'
-import InputAdornment from '@material-ui/core/InputAdornment'; 
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Fab from '@material-ui/core/Fab';
-
-
+import React from "react";
+import {
+  HeaderFlex,
+  ContainerLogo,
+  InputBusca,
+  IconeCarrinho,
+  BuscaInputContainer,
+  IconesContainer,
+} from "./style";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Logo from "../../Logo/logo.png";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Fab from "@material-ui/core/Fab";
 
 class Header extends React.Component {
-    render() {
-        return (
-            <HeaderFlex>
-                <ContainerLogo src={Logo} alt="Logo 4user"/>
-                <div>
-                    <FormControl>
-                        <TextField
-                            label="Perquisar"
-                            variant="outlined"
-                            id="custom-css-outlined-input"
-                        />
-                    </FormControl>
-                </div>
-                
-                <Fab color="primary" aria-label="Add">
-                    <PersonOutlineOutlinedIcon />
-                </Fab>
-                
-                <IconButton>
-                    <Fab color="primary" >
-                        <Badge badgeContent={1}>
-                            <ShoppingCartOutlinedIcon />
-                        </Badge>
-                    </Fab>
-                    
-                </IconButton>
-              
-            </HeaderFlex>
-        )
-    }
+  render() {
+    return (
+      <HeaderFlex>
+        <ContainerLogo src={Logo} alt="Logo 4user" />
+        <BuscaInputContainer>
+          <FormControl>
+            <TextField
+              label="Perquisar"
+              variant="outlined"
+              id="custom-css-outlined-input"
+            />
+          </FormControl>
+        </BuscaInputContainer>
+        <IconesContainer>
+          <Fab color="primary" aria-label="Add">
+            <PersonOutlineOutlinedIcon />
+          </Fab>
+
+          <IconButton>
+            <Fab color="primary">
+              <Badge badgeContent={1}>
+                <ShoppingCartOutlinedIcon />
+              </Badge>
+            </Fab>
+          </IconButton>
+        </IconesContainer>
+      </HeaderFlex>
+    );
+  }
 }
 
-export default Header
+export default Header;
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,27 +1,39 @@
-import styled from 'styled-components'
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input'; 
+import styled from "styled-components";
+import IconButton from "@material-ui/core/IconButton";
+import Input from "@material-ui/core/Input";
 
 export const InputBusca = styled(Input)`
-    background-color: #a5c1ce;
-    border-radius: 10px;
-    :hover{
-        background-color: #BED2DC;
-    }
-`
+  background-color: #a5c1ce;
+  border-radius: 10px;
+  :hover {
+    background-color: #bed2dc;
+  }
+`;
 
 export const BotaoContainer = styled(IconButton)`
-    &&{
-        color: purple;
-    }
-`
+  && {
+    color: purple;
+  }
+`;
 
 export const HeaderFlex = styled.div`
-    display: flex;
-    background-color: #a5c1ce;
-`
+  display: grid;
+  background-color: #a5c1ce;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 4fr 1fr;
+`;
 
 export const ContainerLogo = styled.img`
-    height: 70px;
-    width: auto;
-`
+  height: 70px;
+  width: auto;
+`;
+
+export const BuscaInputContainer = styled.div`
+  margin: auto;
+  width: 40%;
+`;
+
+export const IconesContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
