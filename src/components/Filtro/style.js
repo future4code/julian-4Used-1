3def668f954de7fe220a359c0d9fd1e4dfbd0321
Rafael Cardoso <rafafscardoso@gmail.com>
2020-05-07 13:25:25 -0300

    Lista de produtos com filtro e sort

---
+++ src/components/Filtro/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { 
+  TextField, 
+  FormControl, 
+  InputLabel,
+  Select, 
+  MenuItem, 
+  Button
+} from '@material-ui/core';
+
+export const BarraFiltroContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`
+
+export const FiltroContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const FormContainer = styled.div``
+
+export const FormControle = styled(FormControl)``
+
+export const InputContainer = styled(TextField)``
+
+export const LabelSelect = styled(InputLabel)``
+
+export const SelectContainer = styled(Select)``
+
+export const Option = styled(MenuItem)``
+
+export const Botao = styled(Button)``
+
+export const SortContainer = styled.div``
